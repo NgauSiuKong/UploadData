99ca63fa9b7e5def575f450b68e4c43944a01085
NiuShao.Lenovo <370574131@qq.com>
2017-12-05 21:43:40 +0800

    自动加载以及读取excel类完成

--- App/operate_excel/operate_excel.php
+++ App/operate_excel/operate_excel.php
@@ -1,10 +1,50 @@
 <?php
 namespace App\operate_excel;
     class operate_excel
-    { 
-        public function test()
-        { 
-            echo "operate_excel test hello word";
+    {
+        static $oprtObj;
+        public $PHPExcelObj;
+        private function __construct($Obj)
+        {
+            $this->PHPExcelObj = $Obj;
         }
+        static function getoprtObj($Obj)
+        {
+            //在这里犯错了,直接调用了$oprtObj;
+            if(is_null(self::$oprtObj)){
+                self::$oprtObj = new self($Obj);
+            }
+            return self::$oprtObj;
+        }
+        //文件直接读取方法,全部导出
+        public function readExcel()
+        {
+            $sheetCount = $this->PHPExcelObj->getSheetCount();
+            $res_arr = array();
+            for($i=0;$i<$sheetCount;$i++){
+                $res_arr[$i] = $this->PHPExcelObj->getSheet($i)->toArray();
+            }
+            return $res_arr;
+        }
+        //文件逐行读取,节省内存
+        public function readlineExcel()
+        {
+            $res_arr = array();
+            //遍历每一个sheet
+            foreach($this->PHPExcelObj->getWorksheetIterator() as $key_sheet => $sheet){
+                //遍历的每一个sheet逐行读取
+                foreach($sheet->getRowIterator() as $key_line => $row){
+                    if($row->getRowIndex() == 1){
+                        continue;
+                    }
+                    //遍历的每一个行逐列读取
+                    foreach($row->getCellIterator() as $key_column => $cell){
+                        $data_cell = $cell->getValue();
+                        $res_arr[$key_sheet][$key_line][$key_column] = $data_cell;
+                    }
+                }
+            }
+            return $res_arr;
+        }
+
     }
-    
--- active.php
+++ active.php
@@ -1,2 +1,12 @@
 <?php
-    
+$file = "./file/data.xlsx";
+//phpexcel实例化对象,composer已经完全加载
+$PHPExcelObj = PHPExcel_IOFactory::load($file);
+use App\operate_excel\operate_excel;
+$oprtObj = operate_excel::getoprtObj($PHPExcelObj);
+//$res = $oprtObj->readlineExcel();
+$res = file($file);
+dump($res);
+
+
+
